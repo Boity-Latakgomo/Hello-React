01e65657d83dc6918f16449786c26981b0829c67
Boity-Latakgomo <BoityLatakgomo@gmail.com>
2022-02-09 22:08:53 +0200

    added a decrement button

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -23,8 +23,16 @@ import React, { Component } from 'react';
              <button 
              onClick={() => this.props.onIncrement(this.props.counter) }
              className="btn btn-secondary btn-sm">
-                 Increment Counter
+                 +
                  </button>
+
+                 
+                 <button 
+             onClick={() => this.props.onDecrement(this.props.counter) }
+             className="btn btn-secondary btn-sm">
+                 -
+             </button>
+                
                  <button
                   onClick={() => this.props.onDelete(this.props.counter.id)}
                    className="btn btn-danger btn-sm m-3"
